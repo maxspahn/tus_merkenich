abb038e3da4f3b436dd132b7711c49a1ce1205da
mspahn <m.spahn@tudelft.nl>
2020-03-24 07:53:31 +0100

    Fixes bug for ilmatar images and improves color for visited menu links

---
+++ site/slidesSingle.js
@@ -0,0 +1,69 @@
+var slideIndex = 1;
+var slideName1 = "mainSlide" 
+var dotName1 = "mainDot" 
+showSlides(slideIndex, slideName1, dotName1);
+
+// Next/previous controls
+function plusSlides(n, slideName, dotName) {
+  showSlides(slideIndex += n, slideName, dotName);
+}
+
+// Thumbnail image controls
+function currentSlide(n, slideName, dotName) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n, slideName, dotName) {
+  var i;
+  var slides = document.getElementsByName(slideName);
+  var dots = document.getElementsByName(dotName);
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+} 
+
+function plusSlides(n, slideName, dotName){
+  clearInterval(myTimer);
+  if (n < 0){
+    showSlides(slideIndex -= 1, slideName, dotName);
+  } else {
+    showSlides(slideIndex += 1, slideName, dotName); 
+  }
+  if (n === -1){
+    myTimer = setInterval(function(){plusSlides(n + 2, slideName, dotName)}, 4000);
+  } else {
+    myTimer = setInterval(function(){plusSlides(n + 1, slideName, dotName)}, 4000);
+  }
+}
+
+function plusSlidesSup(n, slideName, dotName){
+  clearInterval(myTimer);
+  if (n < 0){
+    showSlides(slideIndex -= 1, slideName, dotName);
+  } else {
+   showSlides(slideIndex += 1, slideName, dotName); 
+  }
+  if (n === -1){
+    myTimer = setInterval(function(){plusSlides(n + 2, slideName, dotName)}, 4000);
+  } else {
+    myTimer = setInterval(function(){plusSlides(n + 1, slideName, dotName)}, 4000);
+  }
+}
+
+window.addEventListener("load",function() {
+    showSlides(slideIndex, slideName1, dotName1);
+    myTimer = setInterval(function(){plusSlides(1, slideName1, dotName1)}, 4000);
+})
+
+function currentSlide(n, slideName, dotName){
+  clearInterval(myTimer);
+  myTimer = setInterval(function(){plusSlides(n + 1, slideName, dotName)}, 4000);
+  showSlides(slideIndex = n, slideName, dotName);
+}
